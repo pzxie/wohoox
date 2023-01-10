00ff93c2effc222b95520707a564cb5dd4700c8c
pzxie <pzxie@outlook.com>
2023-01-10 16:22:56 +0800

    :construction_worker: build(monorepo): add version management tools - changeset

--- packages/wohoox-react/src/index.ts
+++ packages/wohoox-react/src/index.ts
@@ -1,5 +1,7 @@
 import createStore from 'wohoox'
+
+export default createStore
+
 export { useWohoox as useStore } from './hooks/useWohoox'
 export { combineStores, dispatch, dispatchAll } from 'wohoox'
 export type { WohooxPlugin } from 'wohoox'
-export default createStore
